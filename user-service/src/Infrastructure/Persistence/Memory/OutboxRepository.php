d7f050d6889338638c956788762c383bcd3c5c64
Dmytro Denysov <denisov1985@gmail.com>
2024-06-29 12:18:38 +0300

    Added some env stuff

--- user-service/src/Infrastructure/Persistence/Memory/OutboxRepository.php
+++ user-service/src/Infrastructure/Persistence/Memory/OutboxRepository.php
@@ -2,18 +2,21 @@
 
 namespace User\Infrastructure\Persistence\Memory;
 
+use User\Application\Outbox\Outbox;
 use User\Application\Outbox\OutboxStatus;
 use User\Domain\Model\Event\DomainEvent;
 use User\Infrastructure\Persistence\Memory\Data\OutboxDataset;
 
 class OutboxRepository implements \User\Application\Outbox\OutboxRepository
 {
-    public function save(DomainEvent $event): void
+    public function save(Outbox $outbox): void
     {
         OutboxDataset::$data[] = [
-            'name'    => $event->getName(),
-            'payload' => $event->toArray(),
-            'status'  => OutboxStatus::STARTED,
+            'id'         => $outbox->id,
+            'name'       => $outbox->name,
+            'payload'    => $outbox->payload,
+            'created_at' => $outbox->createdAt,
+            'status'     => OutboxStatus::STARTED,
         ];
     }
 }
