8bc86eb321e0f025048907c878bff8d2eb065b58
Dmytro Denysov <denisov1985@gmail.com>
2024-07-13 13:13:44 +0300

    Added some common CQRS stuff

--- iam-service/src/Delivery/Http/Controller/RegisterController.php
+++ iam-service/src/Delivery/Http/Controller/RegisterController.php
@@ -8,10 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController
 {
+    /**
+     * @param CommandBus $commandBus
+     */
     public function __construct(public CommandBus $commandBus)
     {
     }
 
+    /**
+     * @param Request $request
+     * @return void
+     */
     #[Route('/register', name: 'register', methods: ['POST', 'GET'])]
     public function index(Request $request)
     {
