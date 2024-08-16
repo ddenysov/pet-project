74786b15e9be072ed0ca2c7f20e7ac0829dc68f8
Dmytro Denysov <denisov1985@gmail.com>
2024-08-16 23:18:25 +0300

    Added some frontend stuff

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ["@pinia/nuxt"]
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    modules: [
+        "@pinia/nuxt",
+        '@primevue/nuxt-module'
+    ],
+    primevue: {
+        autoImport: false,
+        components: {
+            include: [
+                'DatePicker',
+                'Select',
+                'Menu',
+                'ProgressBar',
+                'Button',
+                'IconField',
+                'InputIcon',
+                'InputText',
+            ],
+        },
+        options: {
+            theme: {
+                preset: Aura
+            }
+        },
+    },
 })
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
     <h1>Welcome to the index page</h1>
+
+    <Button label="Check" icon="pi pi-check" />
+
+    <i class="pi pi-check"></i>
+    <i class="pi pi-times"></i>
+    <span class="pi pi-search"></span>
+    <span class="pi pi-user"></span>
+
+    <IconField>
+      <InputIcon class="pi pi-search" />
+      <InputText v-model="value1" placeholder="Search" />
+    </IconField>
+
+    <IconField>
+      <InputText v-model="value2" />
+      <InputIcon class="pi pi-spin pi-spinner" />
+    </IconField>
   </div>
 </template>
-<script setup lang="ts">
-</script>
