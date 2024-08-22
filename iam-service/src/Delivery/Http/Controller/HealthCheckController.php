7da0d8870980b14e9ff94c4e4fd4c668d687fecd
Dmytro Denysov <denisov1985@gmail.com>
2024-08-22 18:37:41 +0300

    Added simple cress service logging stuff

--- iam-service/src/Delivery/Http/Controller/HealthCheckController.php
+++ iam-service/src/Delivery/Http/Controller/HealthCheckController.php
@@ -5,13 +5,14 @@ namespace Iam\Delivery\Http\Controller;
 use Common\Application\Bus\Port\CommandBus;
 use Common\Application\Bus\Port\QueryBus;
 use Common\Infrastructure\Delivery\Symfony\Http\Controller;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class HealthCheckController extends Controller
 {
-    public function __construct(CommandBus $commandBus, QueryBus $queryBus)
+    public function __construct(CommandBus $commandBus, QueryBus $queryBus, private LoggerInterface $logger)
     {
         parent::__construct($commandBus, $queryBus);
     }
@@ -22,6 +23,7 @@ class HealthCheckController extends Controller
     #[Route('/health-check', name: 'health-check', methods: ['POST', 'GET'], format: 'json')]
     public function __invoke(): JsonResponse
     {
+        $this->logger->info('IAM: Healthcheck OK');
         return new JsonResponse([
             'ok' => date('Y-m-d H:i:s'),
         ]);
