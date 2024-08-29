ef5414ea38d4f723fffdbb830cdc8d868012491a
Dmytro Denysov <denisov1985@gmail.com>
2024-08-29 21:39:33 +0300

    Added rides service some refactoring stuff

--- ride-service/migrations/Version20240829183837.php
+++ ride-service/migrations/Version20240829183837.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240829152911 extends AbstractMigration
+final class Version20240829183837 extends AbstractMigration
 {
     public function getDescription(): string
     {
