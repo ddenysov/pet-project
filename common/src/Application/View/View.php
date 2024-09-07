df766f354aa43ad962d0e76d9e2941e09d3db265
Dmytro Denysov <denisov1985@gmail.com>
2024-09-07 09:56:47 +0300

    Added some read model refactoring stuff

---
+++ common/src/Application/View/View.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Common\Application\View;
+
+use JsonSerializable;
+
+abstract readonly class View implements JsonSerializable
+{
+    /**
+     * @param array $data
+     */
+    public function __construct(protected array $data = [])
+    {
+    }
+
+    /**
+     * @return array
+     */
+    abstract public function toArray(): array;
+
+    /**
+     * @return mixed
+     */
+    final public function jsonSerialize(): mixed
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * @param string $name
+     * @return mixed|void
+     */
+    final public function __get(string $name)
+    {
+        $data = $this->toArray();
+
+        if (isset($data[$name])) {
+            return $data[$name];
+        }
+    }
+
+    /**
+     * @param array $collection
+     * @return array
+     */
+    final public static function collection(array $collection): array
+    {
+        return array_map(function (array $item) {
+            return new static($item);
+        }, $collection);
+    }
+}
--- ride-service/src/Application/Handlers/Query/RideView.php
+++ ride-service/src/Application/Handlers/Query/RideView.php
@@ -2,42 +2,18 @@
 
 namespace Ride\Application\Handlers\Query;
 
-use Common\Utils\Serialize\Trait\ObjectToArray;
+use Common\Application\View\View;
 
-class RideView
+readonly class RideView extends View
 {
-    use ObjectToArray;
-
-    public string $id;
-
-    public string $name;
-
-    /**
-     * @param string $id
-     * @param string $name
-     */
-    public function __construct(string $id, string $name)
-    {
-        $this->id   = $id;
-        $this->name = $name;
-    }
-
     /**
      * @return array
      */
     public function toArray(): array
     {
-        return $this->propertiesToArray();
-    }
-
-    /**
-     * @param array $collection
-     * @return RideView[]
-     */
-    public static function collection(array $collection)
-    {
-        return array_map(function (array $item) {
-            return new static($item['id'], $item['name']);
-        }, $collection);
+        return [
+            'id'   => $this->data['id'],
+            'name' => $this->data['name'],
+        ];
     }
 }
