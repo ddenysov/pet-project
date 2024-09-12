1e7adcb67733609f697b9dfcc014afc3574deefa
Dmytro Denysov <denisov1985@gmail.com>
2024-09-12 12:26:14 +0300

    Added some frontend stuff

---
+++ frontend/components/ui/card/UiCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+export interface Props {
+  header: string | null,
+  title: string | null,
+  subtitle: string | null,
+  content: string | null,
+  footer: string | null,
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <Card>
+    <template #header>
+      <slot name="header">{{ header }}</slot>
+    </template>
+    <template #title>
+      <slot name="title">{{ title }}</slot>
+    </template>
+    <template #subtitle>
+      <slot name="subtitle">{{ subtitle }}</slot>
+    </template>
+    <template #content>
+      <slot name="content">{{ content }}</slot>
+    </template>
+    <template #footer>
+      <slot name="footer">{{ footer }}</slot>
+    </template>
+  </Card>
+</template>
+
+<style scoped>
+
+</style>
