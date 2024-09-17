bb492ddefe97bf095fea05045887a9d9daef44df
Dmytro Denysov <denisov1985@gmail.com>
2024-09-17 22:57:26 +0300

    Added some frontend stuff

--- ride-service/src/Delivery/Http/Controller/HealthCheckController.php
+++ ride-service/src/Delivery/Http/Controller/HealthCheckController.php
@@ -7,6 +7,8 @@ use Common\Application\Bus\Port\QueryBus;
 use Common\Infrastructure\Delivery\Symfony\Http\Controller;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 use Ride\Application\Handlers\Command\HealthCheckCommand;
 use Ride\Application\Handlers\Query\HealthCheckQuery;
@@ -18,11 +20,18 @@ class HealthCheckController extends Controller
      * @return JsonResponse
      */
     #[Route('/health-check', name: 'health-check', methods: ['POST', 'GET'], format: 'json')]
-    public function __invoke(): JsonResponse
+    public function __invoke(HubInterface $hub): JsonResponse
     {
         $this->logger->info('Healthcheck started');
-        $this->queryBus->execute(new HealthCheckQuery(time()));
-        $this->commandBus->execute(new HealthCheckCommand());
+        //$this->queryBus->execute(new HealthCheckQuery(time()));
+        //$this->commandBus->execute(new HealthCheckCommand());
+
+        $update = new Update(
+            'https://example.com/books/1',
+            json_encode(['status' => 'OutOfStock'])
+        );
+
+        $hub->publish($update);
 
         return new JsonResponse([
             'ok' => date('Y-m-d H:i:s'),
