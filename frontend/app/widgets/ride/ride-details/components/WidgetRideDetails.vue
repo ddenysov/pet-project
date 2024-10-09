1c3b1eac26ea7478b7ee524821b4fa3398e7b032
Dmytro Denysov <denisov1985@gmail.com>
2024-10-09 18:04:15 +0300

    Some frontend refactoring

--- frontend/app/widgets/ride/ride-details/components/WidgetRideDetails.vue
+++ frontend/app/widgets/ride/ride-details/components/WidgetRideDetails.vue
@@ -1,12 +1,68 @@
 <script setup lang="ts">
+import {useRideStore} from "~/app/model/ride/store/ride";
 
-
+const { ride } = useRideStore();
 </script>
 
 <template>
-  <div>ololo trololo</div>
+  <ui-flex gap="6" class="bg-blue-200 p-6">
+    <ui-flex gap="4" direction="column" :grow="1" class="bg-red-200">
+      <ui-flex class="bg-yellow-200 preview-image-container">
+        IMAAAAGE
+      </ui-flex>
+      <ui-flex  class="bg-cyan-700">Title</ui-flex>
+      <ui-flex  class="bg-cyan-700">Description</ui-flex>
+    </ui-flex>
+
+    <ui-flex class="w-4">
+      <ui-flex gap="4" direction="column" :grow="1">
+        <ui-flex direction="column" class="bg-white">
+          <ui-flex gap="2" class="p-3">
+            <ui-flex>
+              <ui-avatar image="/media/sample_avatar.jpg" />
+            </ui-flex>
+            <ui-flex justify-content="between" direction="column" :grow="1">
+              <ui-flex class="p-1">Організатор покатушки:</ui-flex>
+              <ui-flex class="p-1 font-bold">Dima D.</ui-flex>
+            </ui-flex>
+          </ui-flex>
+        </ui-flex>
+
+        <ui-flex direction="column" class="bg-white">
+          <ui-flex gap="2" class="p-3">
+            <ui-flex>
+              <ui-avatar image="/media/sample_avatar.jpg" />
+            </ui-flex>
+            <ui-flex justify-content="between" direction="column" :grow="1">
+              <ui-flex class="p-1">
+                <ui-date-value :value="ride.start_date_time" />
+              </ui-flex>
+              <ui-flex class="p-1 ">
+                Початок: <ui-time-value :value="ride.start_date_time" />, кінець: <ui-time-value :value="ride.end_date_time" />
+              </ui-flex>
+            </ui-flex>
+          </ui-flex>
+        </ui-flex>
+        <ui-flex direction="column" class="bg-white">
+          <ui-flex gap="2" class="p-3">
+            <ui-flex>
+              <ui-avatar image="/media/sample_avatar.jpg" />
+            </ui-flex>
+            <ui-flex justify-content="between" direction="column" :grow="1">
+              <ui-flex class="p-1">Стартова локація:</ui-flex>
+              <ui-flex class="p-1">Біля заправки WOG на окружній</ui-flex>
+            </ui-flex>
+          </ui-flex>
+          <div class="bg-gray-600 h-10rem">Map</div>
+        </ui-flex>
+      </ui-flex>
+    </ui-flex>
+  </ui-flex>
 </template>
 
 <style scoped>
+  .preview-image-container {
+    height: 400px;
+  }
 
 </style>
--- frontend/pages/ride/view/[id].vue
+++ frontend/pages/ride/view/[id].vue
@@ -1,43 +1,11 @@
 <template>
-    <ui-section>
-      <ui-breadcrumbs :items="items" :home="home" />
-    </ui-section>
-
-    <widget-ride-details />
-
-    <ui-section>
-      <div
-        style="border-radius: 1rem; height: 400px; overflow: hidden; display: flex; justify-content: center; align-items: center">
-        <Image style="object-fit: cover; object-position: center" :src="store.ride.image_url" alt="Image" width="100%"
-               height="100%" />
-      </div>
-    </ui-section>
-
-    <ui-section>
-      <h1>{{ store.ride.name }}</h1>
-    </ui-section>
-
-    <ui-flex :gap="4">
-      <ui-flex :grow="1">
-        <div style="background-color: var(--p-surface-100); border-radius: 8px; padding: 1rem">
-          <p>{{ store.ride.description }}</p>
-        </div>
-      </ui-flex>
-      <ui-flex :gap="4" direction="column" class="w-4">
-        <div style="background-color: var(--p-amber-100); border-radius: 8px; padding: 1rem">
-          <div>Дата: <ui-date-value :value="store.ride.start_date_time" /></div>
-          <div>Час початку: <ui-time-value :value="store.ride.start_date_time" /></div>
-          <div>Час фінішу: <ui-time-value :value="store.ride.end_date_time" /></div>
-          <div>Місце початку: Назва місця. <a href="#">Мапа</a></div>
-
-        </div>
-      </ui-flex>
-    </ui-flex>
+  <widget-ride-details />
 </template>
 <script setup lang="ts">
 import {useRideStore} from "~/app/model/ride/store/ride";
 import UiBreadcrumbs from "~/app/ui/components/breadcrumbs/UiBreadcrumbs.vue";
 import WidgetRideDetails from "~/app/widgets/ride/ride-details/components/WidgetRideDetails.vue";
+import FeatureRideForm from "~/app/features/ride/components/FeatureRideForm.vue";
 
 const route = useRoute()
 const store = useRideStore();
