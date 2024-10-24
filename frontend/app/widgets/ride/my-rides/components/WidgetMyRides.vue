c0ba02243f767fab5fdda4b44184f156ba0791ea
Dmytro Denysov <denisov1985@gmail.com>
2024-10-24 23:37:48 +0300

    Some frontend stuff stuff

--- frontend/app/widgets/ride/my-rides/components/WidgetMyRides.vue
+++ frontend/app/widgets/ride/my-rides/components/WidgetMyRides.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card ui-ride-list">
     <DataView paginator :rows="5" :value="products" :layout="layout">
       <template #header>
         <div class="flex justify-end">
@@ -14,7 +14,25 @@
       <template #list="slotProps">
         <ui-panel color="dark" class="my-2">
           <div v-for="(item, index) in slotProps.items" :key="index">
-            <ui-panel color="light" class="m-4 p-6">.</ui-panel>
+            <ui-panel color="light" class="m-4 p-3">
+              <ui-flex justify-content="between">
+                <ui-flex>
+                  <div class="ui-ride-image" />
+                </ui-flex>
+                <ui-flex :gap="2" :grow="1" direction="column" class="px-2">
+                  <ui-flex>
+                    Назва покатушки
+                  </ui-flex>
+                  <ui-flex>
+                    Середа, 12 листопада 2024
+                  </ui-flex>
+                </ui-flex>
+                <ui-flex align-items="center">
+                  <Button text style="height: 40px" type="button" icon="pi pi-ellipsis-v" @click="(e) => toggle(index, e)" aria-haspopup="true" aria-controls="overlay_menu" />
+                  <Menu :key="index" :ref="el => setItemRef(el, index)" id="overlay_menu" :model="items" :popup="true" />
+                </ui-flex>
+              </ui-flex>
+            </ui-panel>
           </div>
         </ui-panel>
       </template>
@@ -30,7 +48,35 @@
 
 <script setup>
 import { ref } from "vue";
+const itemRefs = ref([]);
 
+const setItemRef = (el, index) => {
+  if (el) {
+    itemRefs.value[index] = el;
+  }
+};
+const items = ref([
+  {
+    items: [
+      {
+        label: 'Деталі',
+        icon: 'pi pi-refresh'
+      },
+      {
+        label: 'Скасувати',
+        icon: 'pi pi-upload'
+      },
+      {
+        label: 'Редагувати',
+        icon: 'pi pi-refresh'
+      },
+    ]
+  }
+]);
+
+const toggle = (index, event) => {
+  itemRefs.value[index].toggle(event);
+};
 const products = ref([
   {
     id: '1000',
@@ -150,3 +196,18 @@ const getSeverity = (product) => {
 }
 
 </script>
+
+
+<style scoped>
+.ui-ride-image {
+  background-color: var(--p-surface-300);
+  width: 70px;
+  height: 50px;
+}
+
+.ui-ride-list {
+  .p-dataview-header {
+    border-style: none !important;
+  }
+}
+</style>
