854763183c0168fe169ab087dab6f63ad6e2e3ff
Dmytro Denysov <denisov1985@gmail.com>
2024-10-25 09:47:30 +0300

    Some frontend stuff stuff

---
+++ frontend/app/widgets/track/my-tracks/components/WidgetMyTracks.vue
@@ -0,0 +1,203 @@
+<template>
+  <div class="card ui-ride-list">
+    <DataView paginator :rows="5" :value="products" :layout="layout">
+      <template #list="slotProps">
+        <ui-panel color="dark" class="my-2">
+          <div v-for="(item, index) in slotProps.items" :key="index">
+            <ui-panel color="light" class="m-4 p-3">
+              <ui-flex justify-content="between">
+                <ui-flex>
+                  <div class="ui-ride-image" />
+                </ui-flex>
+                <ui-flex :gap="2" :grow="1" direction="column" class="px-2">
+                  <ui-flex>
+                    Назва треку
+                  </ui-flex>
+                  <ui-flex>
+                    Довжина: 95 км
+                  </ui-flex>
+                </ui-flex>
+                <ui-flex align-items="center">
+                  <Button text style="height: 40px" type="button" icon="pi pi-ellipsis-v" @click="(e) => toggle(index, e)" aria-haspopup="true" aria-controls="overlay_menu" />
+                  <Menu :key="index" :ref="el => setItemRef(el, index)" id="overlay_menu" :model="items" :popup="true" />
+                </ui-flex>
+              </ui-flex>
+            </ui-panel>
+          </div>
+        </ui-panel>
+      </template>
+
+      <template #grid="slotProps">
+        <div>
+          Grid
+        </div>
+      </template>
+    </DataView>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const itemRefs = ref([]);
+
+const setItemRef = (el, index) => {
+  if (el) {
+    itemRefs.value[index] = el;
+  }
+};
+const items = ref([
+  {
+    items: [
+      {
+        label: 'Деталі',
+        icon: 'pi pi-refresh'
+      },
+      {
+        label: 'Скасувати',
+        icon: 'pi pi-upload'
+      },
+      {
+        label: 'Редагувати',
+        icon: 'pi pi-refresh'
+      },
+    ]
+  }
+]);
+
+const toggle = (index, event) => {
+  itemRefs.value[index].toggle(event);
+};
+const products = ref([
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+  },
+
+]);
+const layout = ref('list');
+const options = ref(['list', 'grid']);
+
+const getSeverity = (product) => {
+  switch (product.inventoryStatus) {
+    case 'INSTOCK':
+      return 'success';
+
+    case 'LOWSTOCK':
+      return 'warn';
+
+    case 'OUTOFSTOCK':
+      return 'danger';
+
+    default:
+      return null;
+  }
+}
+
+</script>
+
+
+<style scoped>
+.ui-ride-image {
+  background-color: var(--p-surface-300);
+  width: 70px;
+  height: 50px;
+}
+
+.ui-ride-list {
+  .p-dataview-header {
+    border-style: none !important;
+  }
+}
+</style>
--- frontend/pages/profile/tracks.vue
+++ frontend/pages/profile/tracks.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 import WidgetProfile from "~/app/widgets/profile/components/WidgetProfile.vue";
-import FeatureTrackMap from "~/app/features/track/components/FeatureTrackMap.vue";
+import WidgetMyTracks from "~/app/widgets/track/my-tracks/components/WidgetMyTracks.vue";
 </script>
 
 <template>
@@ -17,6 +17,8 @@ import FeatureTrackMap from "~/app/features/track/components/FeatureTrackMap.vue
           label="Додати маршрут"
           @click="() => navigateTo('/track/create')"
         />
+
+        <widget-my-tracks/>
       </div>
     </template>
   </widget-profile>
