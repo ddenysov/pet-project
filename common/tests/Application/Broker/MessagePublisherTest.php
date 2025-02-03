6bbc828b814abd430a92fa0f864dfc6633acc1fe
Dmytro Denysov <denisov1985@gmail.com>
2025-02-03 13:36:39 +0200

    Refactoring base library

--- common/tests/Application/Broker/MessagePublisherTest.php
+++ common/tests/Application/Broker/MessagePublisherTest.php
@@ -4,24 +4,12 @@ declare(strict_types=1);
 namespace Tests\Application\Broker;
 
 use Common\Application\Broker\MessagePublisher;
-use Common\Application\EventStore\EventStore;
-use Common\Domain\ValueObject\Exception\InvalidUuidException;
-use Common\Domain\ValueObject\Uuid;
 use Common\Infrastructure\Broker\Memory\MessageBroker;
 use Common\Infrastructure\Broker\Memory\MessageOutboxRepository;
-use Common\Infrastructure\EventStore\Memory\EventRepository;
-use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
-use Tests\Mock\Application\Command\StubCreateBlogPostCommand;
-use Tests\Mock\Application\Command\StubCreateBlogPostCommandHandler;
-use Tests\Mock\Domain\Repository\StubBlogPostRepository;
 
 final class MessagePublisherTest extends TestCase
 {
-    /**
-     * @throws InvalidUuidException
-     * @throws Exception
-     */
     public function testCase1(): void
     {
         $messageRepository = new MessageOutboxRepository();
